37b2abd475798bae57d7ff92d9adecd482109314
Rabah Zeineddine <rabah.zeineddine@hotmail.com>
2019-12-12 22:56:32 -0300

    remove auth

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom'
-import PrivateRoute from './PrivateRoute'
-import PublicRoute from './PublicRoute'
-import Login from './Login'
 import Home from './Home'
 import NotFound from './NotFound'
 import 'typeface-roboto';
@@ -11,8 +8,7 @@ class App extends Component {
   render() {
     return (
       <Switch>
-        <PublicRoute exact path="/login" component={Login} />
-        <PrivateRoute exact path="/" component={Home} />
+        <Route exact path="/" component={Home} />
         <Route component={NotFound} />
       </Switch>
     );
